4b9042b80d61f811f9eb8ebd63f1370cf2d3bc35
hansen1416 <badapplesweetie@gmail.com>
2024-03-03 18:25:15 +0800

    1

--- constants.py
+++ constants.py
@@ -1,5 +1,22 @@
 import os
 
-SCREEN_SHOT_DIR = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "..", "video-recorder", "data"
-)
+HOME_DIR = os.path.expanduser("~")
+
+QUEUE_DIR = os.path.join(HOME_DIR, "Documents", "video2motion", "queue")
+
+SCREENSHOT_DIR = os.path.join(HOME_DIR, "Documents", "video2motion", "screenshots")
+
+MEDIAPIPE_DIR = os.path.join(HOME_DIR, "Documents", "video2motion", "mediapipe")
+
+RESNET_DIR = os.path.join(HOME_DIR, "Documents", "video2motion", "resnet")
+
+
+if __name__ == "__main__":
+
+    humanoid = "dors.glb"
+
+    for f in os.listdir(os.path.join(SCREENSHOT_DIR, humanoid)):
+        print(f)
+        # os.remove(os.path.join(SCREEN_SHOT_DIR, f))
+
+        break
---
+++ run_tasks.py
@@ -0,0 +1,17 @@
+from time import sleep
+import time
+import os
+import json
+
+from tqdm import tqdm
+import numpy as np
+from multiprocessing import Process
+from dotenv import load_dotenv
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+
+if __name__ == "__mian__":
+
+    pass
