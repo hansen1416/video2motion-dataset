900a8b3605e97585f02b8aceb75180ca82c68c74
hansen1416 <badapplesweetie@gmail.com>
2024-03-06 19:56:58 +0800

    1

---
+++ OpenposeTask.py
@@ -0,0 +1,32 @@
+import json
+import os
+from multiprocessing import Process
+
+
+def openpose_predict():
+    pass
+
+
+class ResnetTask(Process):
+
+    # override the constructor
+    def __init__(
+        self, queue_file_path, source="local", size_limit=None, model_name="resnet18"
+    ):
+        self.queue_file_path = queue_file_path
+
+    def run(self) -> None:
+
+        with open(self.queue_file_path, "r") as f:
+            queue_data = json.load(f)
+
+        queue_num = int(
+            os.path.basename(self.queue_file_path)
+            .replace(".json", "")
+            .replace("queue", "")
+        )
+
+
+if __name__ == "__main__":
+
+    openpose_predict()
