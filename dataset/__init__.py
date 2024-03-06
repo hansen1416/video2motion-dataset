62a9c478f95acefe11f4bc3a1cfbeaaa42bbfed6
hansen1416 <badapplesweetie@gmail.com>
2024-03-06 19:25:10 +0800

    1

--- dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-from .MediapipeDataset import MediapipeDataset, fetch_datatset_info
-from .FilewiseShuffleSampler import FilewiseShuffleSampler
-
-DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
-
-
-__all__ = [DATA_DIR, MediapipeDataset, FilewiseShuffleSampler, fetch_datatset_info]
