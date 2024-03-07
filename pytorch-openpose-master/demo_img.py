999915a860cf880d74f300f7fcab52adcce52d29
Hanson Han <105476@grundfos.com>
2024-03-07 14:50:45 +0800

    1

---
+++ pytorch-openpose-master/demo_img.py
@@ -0,0 +1,26 @@
+import cv2
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+
+from src import model
+from src import util
+from src.body import Body
+
+body_estimation = Body("model/body_pose_model.pth")
+
+test_image = "images/18.jpg"
+oriImg = cv2.imread(test_image)  # B,G,R order
+candidate, subset = body_estimation(oriImg)
+
+print(candidate)
+print(subset)
+
+# canvas = copy.deepcopy(oriImg)
+# canvas = util.draw_bodypose(canvas, candidate, subset)
+# # detect hand
+
+
+# plt.imshow(canvas[:, :, [2, 1, 0]])
+# plt.axis("off")
+# plt.show()
