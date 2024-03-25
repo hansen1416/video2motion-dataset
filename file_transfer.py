d2f5cea0344bca7dd5d8771602311a56d3292384
hansen1416 <badapllesweetie@gmail.com>
2024-03-25 16:12:29 +0800

    1

--- file_transfer.py
+++ file_transfer.py
@@ -6,14 +6,14 @@ load_dotenv()
 
 if __name__ == "__main__":
 
-    # folder_downloader(
-    #     bucket_name="pose-daten",
-    #     oss_endpoint="oss-ap-southeast-1.aliyuncs.com",
-    #     oss_prefix="detectron2d",
-    #     target_dir=os.path.join(
-    #         os.path.expanduser("~"), "Documents", "video2motion", "detectron2d"
-    #     ),
-    # )
+    folder_downloader(
+        bucket_name="pose-daten",
+        oss_endpoint="oss-ap-southeast-1.aliyuncs.com",
+        oss_prefix="custom_dataset",
+        target_dir=os.path.join(
+            os.path.expanduser("~"), "Documents", "video2motion", "custom_dataset"
+        ),
+    )
 
     folder_downloader(
         bucket_name="pose-daten",
--- readme.md
+++
@@ -1,3 +0,0 @@
-`pip install oss2 torch torchvision mediapipe`
-
-use mediapipe/resnet to generate data from screenshots. use its results to do transfer learning
