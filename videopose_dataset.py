ec7f8439c49960b326346d74622fbbef6dfbc658
hansen1416 <badapllesweetie@gmail.com>
2024-03-26 14:45:11 +0800

    1

---
+++ videopose_dataset.py
@@ -0,0 +1,15 @@
+import os
+import numpy as np
+
+
+if __name__ == "__main__":
+
+    customset_dir = os.path.join(
+        os.path.expanduser("~"), "Documents", "video2motion", "custom_dataset"
+    )
+
+    # Load the dataset
+    data = np.load(os.path.join(customset_dir, "20240322-2086.npz"), allow_pickle=True)
+
+    print(data["positions_2d"])
+    print(data["metadata"])
