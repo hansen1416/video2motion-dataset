7fda823fba610ab30c65a4e9e75b9864b9a69b62
hansen1416 <badapllesweetie@gmail.com>
2024-03-28 13:28:02 +0800

    1

--- file_transfer.py
+++ file_transfer.py
@@ -1,28 +1,31 @@
 import os
-from oss2_uploader import folder_downloader
+from oss2_uploader import folder_downloader, folder_uploader
 from dotenv import load_dotenv
 
 load_dotenv()
 
 if __name__ == "__main__":
 
-    folder_downloader(
-        bucket_name="pose-daten",
-        oss_endpoint="oss-ap-southeast-1.aliyuncs.com",
-        oss_prefix="anim-json/",
-        target_dir=os.path.join(
-            os.path.expanduser("~"),
-            "Documents",
-            "video2motion-animplayer",
-            "public," "anim-json",
-        ),
-    )
-
     # folder_downloader(
     #     bucket_name="pose-daten",
     #     oss_endpoint="oss-ap-southeast-1.aliyuncs.com",
-    #     oss_prefix="results3d",
+    #     oss_prefix="anim-json/",
     #     target_dir=os.path.join(
-    #         os.path.expanduser("~"), "Documents", "video2motion", "results3d"
+    #         os.path.expanduser("~"),
+    #         "Documents",
+    #         "video2motion-animsaver",
+    #         "data",
     #     ),
     # )
+
+    folder_uploader(
+        folder_path=os.path.join(
+            os.path.expanduser("~"),
+            "Documents",
+            "video2motion-animsaver",
+            "data",
+        ),
+        bucket_name="pose-daten",
+        oss_endpoint="oss-ap-southeast-1.aliyuncs.com",
+        oss_path="anim-euler-uniform/",
+    )
