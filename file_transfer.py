e6d05e65e83bbc1580a1073f17f960ec435693fe
hansen1416 <badapplesweetie@gmail.com>
2024-04-04 19:10:11 +0800

    1

--- file_transfer.py
+++ file_transfer.py
@@ -6,27 +6,26 @@ load_dotenv()
 
 if __name__ == "__main__":
 
-    # folder_downloader(
-    #     bucket_name="pose-daten",
-    #     oss_endpoint="oss-ap-southeast-1.aliyuncs.com",
-    #     oss_prefix="anim-json/",
-    #     target_dir=os.path.join(
-    #         os.path.expanduser("~"),
-    #         "Documents",
-    #         "video2motion-animsaver",
-    #         "data",
-    #     ),
-    # )
-
-    folder_uploader(
-        folder_path=os.path.join(
-            os.path.expanduser("~"),
-            "Documents",
-            "video2motion-animplayer",
-            "public",
-            "anim-euler-uniform",
-        ),
+    folder_downloader(
         bucket_name="pose-daten",
         oss_endpoint="oss-ap-southeast-1.aliyuncs.com",
-        oss_path="anim-euler-uniform/",
+        oss_prefix="results3d/",
+        target_dir=os.path.join(
+            "D:",
+            "video2motion",
+            "results3d",
+        ),
     )
+
+    # folder_uploader(
+    #     folder_path=os.path.join(
+    #         os.path.expanduser("~"),
+    #         "Documents",
+    #         "video2motion-animplayer",
+    #         "public",
+    #         "anim-euler-uniform",
+    #     ),
+    #     bucket_name="pose-daten",
+    #     oss_endpoint="oss-ap-southeast-1.aliyuncs.com",
+    #     oss_path="anim-euler-uniform/",
+    # )
