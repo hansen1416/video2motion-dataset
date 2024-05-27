5e70eb1046d2c43dd0065c45d9866126b2e94d9c
hansen1416 <badapplesweetie@gmail.com>
2024-05-27 22:05:04 +0800

    1

--- MediapieVideoEvaludation.py
+++ MediapieVideoEvaludation.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import shutil
+import random
+import string
+import json
 
 import cv2
 import oss2
@@ -11,6 +14,51 @@ from mediapipe.tasks.python import vision
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+def generate_random_string(length=10):
+    """Generates a random string of the specified length.
+
+    Args:
+        length (int, optional): The desired length of the random string. Defaults to 10.
+
+    Returns:
+        str: A random string containing letters and digits.
+    """
+
+    letters_and_digits = string.ascii_letters + string.digits
+    result = "".join(random.choice(letters_and_digits) for i in range(length))
+    return result
+
+
+class OSSObjectTmpReader:
+    def __init__(self, object_name, bucket):
+        self.object_name = object_name
+        self.bucket = bucket
+
+    def __enter__(self):
+        object_stream = self.bucket.get_object(self.object_name)
+
+        extension = self.object_name.split(".")[-1]
+
+        tmp_video_dir = os.path.join(".", "tmp")
+
+        # create the directory if it does not exist
+        os.makedirs(tmp_video_dir, exist_ok=True)
+
+        # generate a random string for filename
+        video_filename = generate_random_string() + "." + extension
+
+        self.tmp_video_path = os.path.join(tmp_video_dir, video_filename)
+
+        with open(self.tmp_video_path, "wb") as local_fileobj:
+            shutil.copyfileobj(object_stream, local_fileobj)
+
+        return self.tmp_video_path
+
+    def __exit__(self, *args):
+        # remove the temporary video file `self.tmp_video_path`
+        os.remove(self.tmp_video_path)
 
 
 class MediapieVideoEvaludation:
@@ -26,83 +74,90 @@ class MediapieVideoEvaludation:
         # 填写Bucket名称，并设置连接超时时间为30秒。
         self.bucket = oss2.Bucket(auth, endpoint, "pose-daten", connect_timeout=30)
 
-    def evaluate_video(self, video_filename):
+    def check_frames(self, limit=10):
 
-        object_name = f"videos/{video_filename}"
+        counter = 0
 
-        # bucket.get_object的返回值是一个类文件对象（File-Like Object），同时也是一个可迭代对象（Iterable）。
-        # 填写Object的完整路径。Object完整路径中不能包含Bucket名称。
-        object_stream = self.bucket.get_object(object_name)
+        # 列举fun文件夹下的所有文件，包括子目录下的文件。
+        for obj in oss2.ObjectIterator(self.bucket, prefix="anim-euler-uniform/"):
+            animation_name = obj.key.split("/")[-1].split(".")[0]
 
-        # content = object_stream.read()
+            with OSSObjectTmpReader(obj.key, self.bucket) as tmp_file:
+                with open(tmp_file, "r") as f:
+                    json_data = json.load(f)
 
-        tmp_video_dir = os.path.join("tmp", "videos")
+                    frame_lenth = len(json_data["Hips"])
 
-        # create the directory if it does not exist
-        os.makedirs(tmp_video_dir, exist_ok=True)
+            with OSSObjectTmpReader(
+                f"videos/{animation_name}-30-0.avi", self.bucket
+            ) as tmp_video_file:
 
-        # print(sys.getsizeof(content))
+                # Use OpenCV’s VideoCapture to load the input video.
+                video_capture = cv2.VideoCapture(tmp_video_file)
 
-        tmp_video_path = os.path.join(tmp_video_dir, video_filename)
+                num_frames = video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
 
-        with open(tmp_video_path, "wb") as local_fileobj:
-            shutil.copyfileobj(object_stream, local_fileobj)
+                video_capture.release()
 
-        # Use OpenCV’s VideoCapture to load the input video.
-        video_capture = cv2.VideoCapture(tmp_video_path)
+            assert (
+                frame_lenth == num_frames
+            ), f"Frame length does not match the video length. {frame_lenth} != {num_frames}, {animation_name}"
 
-        # Load the frame rate of the video using OpenCV’s CV_CAP_PROP_FPS
-        # You’ll need it to calculate the timestamp for each frame.
-        fps = video_capture.get(cv2.CAP_PROP_FPS)
+            counter += 1
 
-        # Loop through each frame in the video using VideoCapture#read()
-        while video_capture.isOpened():
-            num_frames = video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
+            if counter > limit:
+                break
 
-            print(f"Number of frames: {num_frames}")
+    def evaluate_video(self, animation_name):
 
-            # # Read the frame using VideoCapture#read()
-            # ret, frame = video_capture.read()
+        object_name = f"videos/{animation_name}-30-0.avi"
 
-            # # Break the loop if the video has ended
-            # if not ret:
-            #     break
+        # # Loop through each frame in the video using VideoCapture#read()
+        # while video_capture.isOpened():
+        #     num_frames = video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
 
-            # # Convert the frame to RGB using cv2.COLOR_BGR2RGB
-            # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # # Read the frame using VideoCapture#read()
+        # ret, frame = video_capture.read()
 
-            # # Convert the frame to a MediaPipe’s Image object
-            # mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+        # # Break the loop if the video has ended
+        # if not ret:
+        #     break
 
-            # # Process the image using the MediaPipe instance
-            # # results = mediapipe_instance.process(mp_image)
+        # # Convert the frame to RGB using cv2.COLOR_BGR2RGB
+        # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # # Convert the MediaPipe results to a dictionary
-            # # results_dict = python.solutions.drawing_utils._draw_landmarks(
-            # #     frame, results.pose_landmarks, None, None, None, None
-            # # )
+        # # Convert the frame to a MediaPipe’s Image object
+        # mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
 
-            # # Draw the results on the frame using cv2.imshow()
-            # # cv2.imshow("MediaPipe Pose", results_dict)
+        # # Process the image using the MediaPipe instance
+        # # results = mediapipe_instance.process(mp_image)
 
-            # # Break the loop if the 'q' key is pressed
-            # if cv2.waitKey(1) & 0xFF == ord("q"):
-            #     break
+        # # Convert the MediaPipe results to a dictionary
+        # # results_dict = python.solutions.drawing_utils._draw_landmarks(
+        # #     frame, results.pose_landmarks, None, None, None, None
+        # # )
+
+        # # Draw the results on the frame using cv2.imshow()
+        # # cv2.imshow("MediaPipe Pose", results_dict)
+
+        # # Break the loop if the 'q' key is pressed
+        # if cv2.waitKey(1) & 0xFF == ord("q"):
+        #     break
 
         # # Convert the frame received from OpenCV to a MediaPipe’s Image object.
         # mp_image = mp.Image(
         #     image_format=mp.ImageFormat.SRGB, data=numpy_frame_from_opencv
         # )
 
-        video_capture.release()
-
 
 if __name__ == "__main__":
 
-    video_filename = "Aiming-30-0.avi"
+    video_filename = "Aiming"
 
     mediapie_video_evaludation = MediapieVideoEvaludation()
-    mediapie_video_evaludation.evaluate_video(video_filename)
+    # mediapie_video_evaludation.evaluate_video(video_filename)
+
+    mediapie_video_evaludation.check_frames(limit=10000)
 
 exit()
 
